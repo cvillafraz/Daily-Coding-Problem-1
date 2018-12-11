a9676c67198c51eb797483ad8bb740c24ab715f1
cvillafraz <ciro1200@gmail.com>
2018-12-11 18:57:11 -0430

    Added comments

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 def check_sum(arr,K):
     check=False
+    ### Iterate through the arrey from left to rigth
     for i in range(0,len(arr)):
+        ### Check if current element is equal to K
         if arr[i] == K:
             check=True
             break
         else:
+            ### Iterate through the array from right to left
             for j in reversed(range(i+1,len(arr))):
+                ### Sum each element to its next elements and compare to K
                 if (arr[i] + arr[j] == K):
                     check=True
                     break
